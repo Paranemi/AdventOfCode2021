85a66c4ec4171a6c96a02143911660f661611127
Ludwig Pfann <70544031+Paranemi@users.noreply.github.com>
2021-12-09 17:21:48 +0100

    initial commit

---
+++ PlayGround/PlayGround/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace PlayGround
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            
+
+        }
+    }
+}
